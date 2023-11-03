4dbf345871945bd7e01c8b945f52fe79c7906ccd
xgao-ksm <57019321+xgao-ksm@users.noreply.github.com>
2023-11-03 05:12:10 -0400

    Update downloadblob.sh

--- downloadblob.sh
+++ downloadblob.sh
@@ -20,7 +20,7 @@ export blob_name1=$4
 export blob_name2=$5
 
 # Downloading Blob
-mkdir flaskwebapp
+sudo mkdir flaskwebapp
 az storage blob download --container-name $container_name --name $blob_name1 --file flaskwebapp/$blob_name1 --output table
 az storage blob download --container-name $container_name --name $blob_name2 --file flaskwebapp/$blob_name2 --output table
 az storage blob list --container-name $container_name --output table
